24413ed2380128a53b67b3763ace42b6f25878e6
sskoo <sskoo@SALMANHP>
2025-12-28 20:34:12 +0330

    Add project files.

---
+++ CleanArchProject.Domain/Interfaces/IproductRepository.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchProject.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchProject.Domain.Interfaces
+{
+    public interface IproductRepository
+    {
+        IEnumerable<Product> GetAll();
+        Product get(int id);
+        void Add(Product product);
+        //bool add(Product product);
+        void Edit(Product product);
+
+        void Delete(Product product);
+
+        bool Delete(int id);
+
+        void DeleteAll();
+
+    }
+}
---
+++ CleanArchProject.Domain/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchProject.Domain.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDeleted { get; set; }
+        public  ICollection<Product> Products{ get; set; }
+        
+
+    }
+}
