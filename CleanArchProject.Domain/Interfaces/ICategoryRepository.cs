f43be59a28740e979beaaee47cddab2230b40822
Salman Koohestani <sskooh@gmail.com>
2025-12-29 01:00:04 +0330

    i made i category for interface###

--- CleanArchProject.Domain/Interfaces/ICategoryRepository.cs
+++ CleanArchProject.Domain/Interfaces/ICategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchProject.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,12 @@ namespace CleanArchProject.Domain.Interfaces
 {
     public interface ICategoryRepository
     {
+        IEnumerable<Category> GetAll ();
+        Category GetById (int id);
+        void Add(Category category);
+        void Update(Category category);
+        void Delete(int id);
+        bool DeleteAll();
 
     }
 }
---
+++ CleanArchProject.Infra.Data/Context/CleanArchDataBaseContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace CleanArchProject.Infra.Data.Context
+{
+    //public class CleanArchDataBaseContext : Dbcontext
+    //{
+
+    //    //public CleanArchDataBaseContext(DbContextOptions options) : base(options)
+    //    //{
+
+    //    //}
+    //    //public Dbset<product>
+
+    //}
+}
